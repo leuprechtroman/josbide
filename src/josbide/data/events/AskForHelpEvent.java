0769102d80a16fdb29c713ad1638b83116c8ccce
Roman Leuprecht <leuprechtroman@googlemail.com>
2014-09-14 21:29:10 +0200

    Implemented AskForHelpEvent

---
+++ src/josbide/data/events/AskForHelpEvent.java
@@ -0,0 +1,19 @@
+package josbide.data.events;
+
+public class AskForHelpEvent extends OsbideEvent {
+	private String name;
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+}
