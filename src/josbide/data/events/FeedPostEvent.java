d2cd6aeea14bf7be268e3d8e29a1fa7273258b00
Roman Leuprecht <leuprechtroman@googlemail.com>
2014-09-14 21:51:21 +0200

    Implemented FeedPostEvent

---
+++ src/josbide/data/events/FeedPostEvent.java
@@ -0,0 +1,33 @@
+package josbide.data.events;
+
+public class FeedPostEvent extends OsbideEvent {
+	
+	private String name;
+	private String comment;
+	
+	
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the comment
+	 */
+	public String getComment() {
+		return comment;
+	}
+	/**
+	 * @param comment the comment to set
+	 */
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+}
