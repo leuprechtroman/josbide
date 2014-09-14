e51fc5b02d1b151a78a1a8d8caac5fed1de2bb88
Roman Leuprecht <leuprechtroman@googlemail.com>
2014-09-14 21:28:07 +0200

    Implemented EditorActivityEvent

---
+++ src/josbide/data/events/EditorActivityEvent.java
@@ -0,0 +1,21 @@
+package josbide.data.events;
+
+public class EditorActivityEvent extends OsbideEvent {
+	private String name;
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
